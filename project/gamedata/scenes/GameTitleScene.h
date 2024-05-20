6f11b57c32167c30f1df11b6d34ce5fa85648557
KobayashiHirotaka <K022G0013@g.neec.ac.jp>
2024-05-20 15:47:44 +0900

    TitleScene完成

--- project/gamedata/scenes/GameTitleScene.h
+++ project/gamedata/scenes/GameTitleScene.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "Iscene.h"
 #include "CJEngine.h"
+#include "Audio.h"
 #include "Input.h"
+#include "TextureManager.h"
+#include "components/2d/CreateSprite.h"
 
 class GameTitleScene :public Iscene{
 public:
@@ -14,4 +17,25 @@ private:
 	CitrusJunosEngine* CJEngine_;
 
 	Input* input_ = nullptr;
+
+	TextureManager* textureManager_;
+
+	Audio* audio_;
+
+	//audio
+	SoundData bgmData_;
+	SoundData selectData_;
+
+	//sprite
+	std::unique_ptr <CreateSprite> sprite_[2];
+	EulerTransform spriteTransform_[2];
+	EulerTransform SpriteuvTransform_[2];
+	Vector4 spriteMaterial_[2];
+
+	uint32_t titleResource_;
+	uint32_t title1Resource_;
+
+	bool isSpriteDraw_[2];
+
+	int titleTimer_ = 60;
 };
